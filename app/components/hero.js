588284f6c819e02c384f36526cc8071baa49b63c
Mary Welcey Guillen <welceyg@gmail.com>
2025-08-19 19:03:27 -0300

    hero cmp update

--- app/components/hero.js
+++ app/components/hero.js
@@ -5,7 +5,7 @@ export default class HeroComponent extends Component {
     didInsertElement() {
         super.didInsertElement();
         gsap.fromTo(
-            '.text-reveal',
+            '#hero-section .text-reveal',
             { yPercent: "200", rotateZ: 10, opacity: 0 },
             { 
                 yPercent: "0", rotateZ: 0, opacity: 1, duration: 1, ease: "power4",
