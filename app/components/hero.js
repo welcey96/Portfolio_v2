43cb7176c990878b38f7906c2f8e72caa48c27e2
Mary Welcey Guillen <welceyg@gmail.com>
2025-08-20 21:22:52 -0300

    updated hero cmp

--- app/components/hero.js
+++ app/components/hero.js
@@ -1,16 +1,20 @@
 import Component from '@ember/component';
-import { gsap } from "gsap/all";
+import { gsap } from 'gsap/all';
 
 export default class HeroComponent extends Component {
-    didInsertElement() {
-        super.didInsertElement();
-        gsap.fromTo(
-            '#hero-section .text-reveal',
-            { yPercent: "200", rotateZ: 10, opacity: 0 },
-            { 
-                yPercent: "0", rotateZ: 0, opacity: 1, duration: 1, ease: "power4",
-                stagger: 0.3
-            }
-          );
-    }
+  didInsertElement() {
+    super.didInsertElement();
+    gsap.fromTo(
+      '#hero-section .text-reveal',
+      { yPercent: '200', rotateZ: 10, opacity: 0 },
+      {
+        yPercent: '0',
+        rotateZ: 0,
+        opacity: 1,
+        duration: 1,
+        ease: 'power4',
+        stagger: 0.3,
+      },
+    );
+  }
 }
