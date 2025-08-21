bf3c053c3ec54286c3c83172b2c618b02740bf96
Mary Welcey Guillen <welceyg@gmail.com>
2025-08-20 21:22:02 -0300

    navbar updates

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -1,19 +1,18 @@
 import Lenis from '@studio-freight/lenis';
 import Component from '@ember/component';
-import Utils from "../utils/util";
+import Utils from '../utils/util';
 
 export default class NavbarComponent extends Component {
-    didInsertElement() {
-        const $ = new Utils();
-        const lenis = new Lenis();
-        const navLinks = $._qsa("#navbar .menu a");
-        navLinks.forEach(link => {
-            link.addEventListener("click", event => {
-
-                const targetId = $._ga(link, 'href');
-                console.log(targetId)
-                lenis.scrollTo(targetId);
-            });
-        });
-    }
+  didInsertElement() {
+    super.didInsertElement(...arguments);
+    const $ = new Utils();
+    const lenis = new Lenis();
+    const navLinks = $._qsa('#navbar .menu a');
+    navLinks.forEach((link) => {
+      link.addEventListener('click', (event) => {
+        const targetId = $._ga(link, 'href');
+        lenis.scrollTo(targetId);
+      });
+    });
+  }
 }
