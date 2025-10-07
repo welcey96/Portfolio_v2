f954daad1fe231bcc8922c583fc3461a308cb7fc
Mary Welcey Guillen <welceyg@gmail.com>
2025-10-07 20:43:17 -0300

    turn off fingerprint

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,10 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
-    tests: false
+    tests: false,
+    fingerprint: {
+      exclude: ['assets'],
+    },
   });
 
   return app.toTree();
